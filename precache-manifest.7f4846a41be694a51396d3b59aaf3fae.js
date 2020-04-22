128abad1687fd66827b06da74213a72aced202a0
Yevhenii Ihonin <Horugv16@gmail.com>
2020-04-22 17:34:46 +0300

    Updates

---
+++ precache-manifest.7f4846a41be694a51396d3b59aaf3fae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aab58ce369a11e47fa1dd6d8ab2f686b",
+    "url": "/dom-ria-api/index.html"
+  },
+  {
+    "revision": "b5354605e0a8cfa48b8b",
+    "url": "/dom-ria-api/static/css/main.0dc2ad5e.chunk.css"
+  },
+  {
+    "revision": "63c126684d0c55b98593",
+    "url": "/dom-ria-api/static/js/2.29fa37cd.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/dom-ria-api/static/js/2.29fa37cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5354605e0a8cfa48b8b",
+    "url": "/dom-ria-api/static/js/main.af58f1ff.chunk.js"
+  },
+  {
+    "revision": "dea7c2e4e7f8a05a9364",
+    "url": "/dom-ria-api/static/js/runtime-main.296293c0.js"
+  }
+]);
